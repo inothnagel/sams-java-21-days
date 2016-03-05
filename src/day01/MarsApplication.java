76a9829bcfc2d37cd18a4ce8ee7403e9ccbac457
Ignatius Nothnagel <inothnagel@gmail.com>
2016-03-05 21:22:50 +0200

    Add day 1 code

--- src/day01/MarsApplication.java
+++ src/day01/MarsApplication.java
@@ -1,7 +1,23 @@
-package com.thoughtomaton.SamsJava21Days.day01;
+package day01;
 
 class MarsApplication {
     public static void main(String[] args) {
-        System.out.println("hello robot");
+        MarsRobot spirit = new MarsRobot();
+        spirit.status = "exploring";
+        spirit.speed = 2;
+        spirit.temperature = -60;
+        spirit.showAttributes();
+
+        System.out.println("Increasing speed to 3.");
+        spirit.speed = 3;
+        spirit.showAttributes();
+
+        System.out.println("Increasing temperature to -90.");
+        spirit.temperature = -90;
+        spirit.showAttributes();
+
+        spirit.checkTemperature();
+        spirit.showAttributes();
+
     }
 }
